8fc5e91d611d9dec290357fe77c3bc26c8a5f126
Tony Seebregts <tony.seebregts@gmail.com>
2021-07-06 14:13:28 -0700

    Commonalised options

--- cmd/wav2mp4/options/settings.go
+++ cmd/wav2mp4/options/settings.go
@@ -2,8 +2,6 @@ package options
 
 import (
 	"encoding/json"
-	"fmt"
-	"image/color"
 	"os"
 
 	"github.com/transcriptaze/wav2png/cmd/options"
@@ -31,17 +29,4 @@ func (s *settings) Load(file string) error {
 	}
 
 	return nil
-}
-
-func colour(s string) color.NRGBA {
-	var red uint8
-	var green uint8
-	var blue uint8
-	var alpha uint8
-
-	if _, err := fmt.Sscanf(s, "#%02x%02x%02x%02x", &red, &green, &blue, &alpha); err == nil {
-		return color.NRGBA{R: red, G: green, B: blue, A: alpha}
-	}
-
-	return color.NRGBA{R: 0, G: 128, B: 0, A: 255}
 }
