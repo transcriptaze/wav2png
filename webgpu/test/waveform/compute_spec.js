6a6ef8f36c67f93366a9a471258c29d4f27ed89a
twystd <twystd@users.noreply.github.com>
2023-09-26 19:31:54 -0700

    (interim) pixels buckets test for slice@1ms

--- webgpu/test/waveform/compute_spec.js
+++ webgpu/test/waveform/compute_spec.js
@@ -98,10 +98,6 @@ describe('audio pixel bucket logic', function () {
     expect(buckets.length).to.equal(pixels)
     expect(stride).to.be.closeTo(23.4, 0.1)
 
-    // ... check first/last indices
-    expect(buckets.at(0).start).to.equal(start)
-    expect(buckets.at(-1).end).to.equal(end)
-
     // ... check index continuity
     for (let i = 1; i < buckets.length; i++) {
       const p = buckets[i - 1]
@@ -128,8 +124,123 @@ describe('audio pixel bucket logic', function () {
     }
 
     // ... sanity check
+    expect(buckets.at(0).start).to.equal(start)
+    expect(buckets.at(-1).end).to.equal(end)
+
     expect(buckets[0].value).to.be.closeTo(25.3, 0.00001)
     expect(buckets[1879].value).to.be.closeTo(101402.4, 0.00001)
+  })
+
+  it.only('slice @ 1ms', function () {
+    const N = 44100
+    const pixels = width - 2 * padding
+    const stride = N / pixels
+
+    // ... reference slice @ 0ms
+    const slice0ms = []
+
+    {
+      const start = 0
+      for (let i = 0; i < pixels; i++) {
+        const from = Math.round(start + i * stride)
+        const to = Math.round((i + 1) * stride)
+        const value = samples.slice(from, to).reduce((v, a) => a + v)
+
+        slice0ms.push({
+          start: from,
+          end: to,
+          value
+        })
+      }
+    }
+
+    // ... slice @ 1ms
+    const slice1ms = []
+
+    {
+      const start = 0
+      for (let i = 0; i < pixels; i++) {
+        const from = Math.round(start + i * stride)
+        const to = Math.round((i + 1) * stride)
+        const value = samples.slice(from, to).reduce((v, a) => a + v)
+
+        slice1ms.push({
+          start: from,
+          end: to,
+          value
+        })
+      }
+    }
+
+    // ... check setup
+    expect(pixels).to.equal(1880)
+    expect(slice0ms.length).to.equal(pixels)
+    expect(slice1ms.length).to.equal(pixels)
+    expect(stride).to.be.closeTo(23.4, 0.1)
+
+    // ... check index continuity
+    for (let i = 1; i < slice0ms.length; i++) {
+      const p = slice0ms[i - 1]
+      const q = slice0ms[i]
+
+      expect(q[0]).to.equal(p[1])
+    }
+
+    for (let i = 1; i < slice1ms.length; i++) {
+      const p = slice1ms[i - 1]
+      const q = slice1ms[i]
+
+      expect(q[0]).to.equal(p[1])
+    }
+
+    // ... check index stride
+    const floor = Math.floor(stride)
+    const ceil = Math.ceil(stride)
+
+    for (const v of slice0ms) {
+      expect(v.end - v.start).to.be.oneOf([floor, ceil])
+    }
+
+    for (const v of slice1ms) {
+      expect(v.end - v.start).to.be.oneOf([floor, ceil])
+    }
+
+    // ... check pixel values
+    for (let i = 0; i < pixels; i++) {
+      const pixel = slice0ms[i]
+      const N = pixel.end - pixel.start
+      const v = N * (samples[pixel.start] + samples[pixel.end - 1]) / 2
+
+      expect(pixel.value, `${i}`).to.be.closeTo(v, 0.00001)
+    }
+
+    for (let i = 0; i < pixels; i++) {
+      const pixel = slice1ms[i]
+      const N = pixel.end - pixel.start
+      const v = N * (samples[pixel.start] + samples[pixel.end - 1]) / 2
+
+      expect(pixel.value, `${i}`).to.be.closeTo(v, 0.00001)
+    }
+
+    for (let i = 0; i < pixels; i++) {
+      const pixel0 = slice0ms[i]
+      const pixel1 = slice1ms[i]
+
+      expect(pixel1).to.deep.equal(pixel0)
+    }
+
+    // ... sanity check
+    expect(slice0ms.at(0).start).to.equal(0)
+    expect(slice0ms.at(-1).end).to.equal(44100)
+
+    expect(slice0ms[0].value).to.be.closeTo(25.3, 0.00001)
+    expect(slice0ms[1879].value).to.be.closeTo(101402.4, 0.00001)
+
+    expect(slice1ms.at(0).start).to.equal(0)
+    expect(slice1ms.at(-1).end).to.equal(44100)
+
+    expect(slice1ms[0].value).to.be.closeTo(25.3, 0.00001)
+    expect(slice1ms[1879].value).to.be.closeTo(101402.4, 0.00001)
   })
 })
 
